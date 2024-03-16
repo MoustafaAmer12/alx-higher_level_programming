7032c76b5c5fae287dcece7e40182a8ca58943ca
MoustafaAmer12 <moustafaesam20113@gmail.com>
2024-03-16 23:07:36 +0200

    Task 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - recursive palindrome
+ * @head: haed of list
+ *
+ * Return 1 if palindrome and 0 ow
+ */
+int is_palindrome(listint_t **head)
+{
+	if (!head || !*head)
+		return (1);
+	return (palindrome(head, *head));
+}
+/**
+ * palindrome - checks if palindrome or not
+ * @head: head of linked list
+ * @tail: tail of linked list
+ *
+ * Return: 1 if palindrome 0 ow
+ */
+int palindrome(listint_t **head, listint_t *tail)
+{
+	if (!tail)
+		return (1);
+	if (palindrome(head, tail->next) && (*head)-n == tail->n)
+	{
+		*head = (*head)->next;
+		return (1);
+	}
+	return (0);
+}
