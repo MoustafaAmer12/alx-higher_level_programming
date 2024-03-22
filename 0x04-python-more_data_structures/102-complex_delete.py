19b096e0f624f995231ab2bc65ef2d32d044a5b2
MoustafaAmer12 <moustafaesam20113@gmail.com>
2024-03-22 21:54:03 +0200

    Task 5 edits

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,8 +2,7 @@
 def complex_delete(a_dictionary, value):
     if not a_dictionary:
         return
-    new_dict = {}
-    for k, v in a_dictionary.items():
-        if v != value:
-            new_dict[k] = v
-    return new_dict
+    for k in list(a_dictionary.keys()):
+        if a_dictionary[k] == value:
+            a_dictionary.pop(k)
+    return a_dictionary
