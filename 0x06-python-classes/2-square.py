ffa883da8867d928273680684043b9d194666341
MoustafaAmer12 <moustafaesam20113@gmail.com>
2024-04-16 19:25:15 +0200

    Task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A Module that creates and identifies user defined square objects with sizes"""
+
+
+class Square:
+    """A class for creating squares with specific sizes"""
+    def __init__(self, size=0):
+        """Constructor for the class
+        Attributes:
+            size: size of the square to be initialized"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
