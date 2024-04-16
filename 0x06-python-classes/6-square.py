6a0c0eeb5922c4d0b54db891383072b9a0b557f0
MoustafaAmer12 <moustafaesam20113@gmail.com>
2024-04-16 19:59:55 +0200

    Task 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""A module designated for creating and initializing squares"""
+
+
+class Square:
+    """A Class that creates a square with a given size"""
+    def __init__(self, size=0, position=(0, 0)):
+        """Constructor for the class
+        Attributes:
+            size: size of the square to be initialized"""
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculates the area of the square
+        Returns:
+            Area"""
+        return pow(self.__size, 2)
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        err = False
+        count = 0
+        for i in value:
+            count += 1
+            if type(i) is not int or i < 0:
+                err = True
+        if count != 2:
+            err = True
+        if err:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def my_print(self):
+        """Prints a square with # representation"""
+        for i in range(self.size):
+            for j in range(self.position[0]):
+                print(" ", end="")
+            for j in range(self.size):
+                print("#", end="")
+            if i != self.size - 1:
+                print()
+        print()
