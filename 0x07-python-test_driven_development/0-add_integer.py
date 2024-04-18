399061a967f920f710339fd98523cc358282816d
MoustafaAmer12 <moustafaesam20113@gmail.com>
2024-04-18 19:45:02 +0200

    Task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module that adds two integers
+add_integer:
+    adds two given integers as params
+"""
+
+
+def add_integer(a, b=98):
+    """Adds two integers
+    Returns:
+        sum of two integers"""
+    if type(a) is not float and type(a) is not int:
+        raise TypeError("a must be an integer")
+    if type(b) is not float and type(b) is not int:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,4 @@
+# Test Driven Development
+## 0x07 - Python, Test Driven Dev
+This directory is designated for implementing proper testing for modules and classes.
+Tests performed in this task are either doctests or unittests.
