1c94fd234792406821571dcf54c56c9dd2b3c7d8
MoustafaAmer12 <moustafaesam20113@gmail.com>
2024-04-18 16:14:21 +0200

    Task 9

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""A module designated for creating and initializing squares"""
+
+
+class Square:
+    """A Class that creates a square with a given size"""
+    def __init__(self, size=0):
+        """Constructor for the class
+        Attributes:
+            size: size of the square to be initialized"""
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int) and not isinstance(value, float):
+            raise TypeError("size must be a number")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculates the area of the square
+        Returns:
+            Area"""
+        return self.size ** 2
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+    
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
