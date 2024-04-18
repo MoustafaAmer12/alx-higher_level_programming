3f1bf8b61c087911769a1caacd20aff0f9716349
MoustafaAmer12 <moustafaesam20113@gmail.com>
2024-04-18 19:46:25 +0200

    Task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -14,4 +14,8 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) is not float and type(b) is not int:
         raise TypeError("b must be an integer")
+    if a < 0:
+        a = ceil(a)
+    if b < 0:
+        b = ceil(b)
     return int(a) + int(b)
