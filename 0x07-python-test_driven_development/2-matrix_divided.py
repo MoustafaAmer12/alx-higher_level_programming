4c931f7329135aef5394a1ce39b7c1febb4c3774
MoustafaAmer12 <moustafaesam20113@gmail.com>
2024-04-19 14:13:23 +0200

    Task 2 edits

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,21 +1,32 @@
 #!/usr/bin/python3
 """
+Matrix divided Module includes function for dividing
+a matrix with a scalar
+matrix_divided
 """
 
 
 def matrix_divided(matrix, div):
+    """
+    Returns:
+        new_matrix with the result of the divided matrix
+    """
     if type(matrix) is not list or type(matrix[0]) is not list:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError('matrix must be a matrix \
+(list of lists) of integers/floats')
     size = len(matrix[0])
     for i in range(len(matrix)):
         if len(matrix[i]) != size:
             raise TypeError("Each row of the matrix must have the same size")
         for j in range(size):
-            if type(matrix[i][j]) is not int and type(matrix[i][j]) is not float:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            if type(matrix[i][j]) is not int and\
+                    type(matrix[i][j]) is not float:
+                raise TypeError("matrix must be a matrix \
+(list of lists) of integers/floats")
     if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    new_matrix = [[round(matrix[i][j] / div, 2) for j in range(size)] for i in range(len(matrix))]
+    new_matrix = [[round(matrix[i][j] / div, 2) for j in range(size)]
+                  for i in range(len(matrix))]
     return new_matrix
