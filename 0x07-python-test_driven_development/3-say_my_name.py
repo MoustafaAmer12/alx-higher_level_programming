e79749584d7fa50fc647d2a86c20eadf20bbba1a
MoustafaAmer12 <moustafaesam20113@gmail.com>
2024-04-20 18:55:44 +0200

    Task 2 doc

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """
+Module That has a function that lists the full name
+say_my_name: prints the full name
+of given parameters
 """
 
 
 def say_my_name(first_name, last_name=""):
-    """
-    """
+    """Prints the full name of the given arguments
+    Returns:
+        None"""
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
