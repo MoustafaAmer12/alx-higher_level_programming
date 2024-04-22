b492d96736af4944c87326333136f7f778970cd6
MoustafaAmer12 <moustafaesam20113@gmail.com>
2024-04-22 19:11:44 +0200

    Task 7

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,47 +1,15 @@
 #!/usr/bin/python3
 """
-Module that performs matrix multiplication
+Module that performs matrix multiplication by numpy
 matrix_mul:
     takes 2 matrices and returns their product
 """
 import numpy as np
 
+
 def lazy_matrix_mul(m_a, m_b):
-    """
+    """Multiplies 2 Matrices using numpy module
     Returns:
         the product of 2 matrices
     """
-    if type(m_a) is not list:
-        raise TypeError("m_a must be a list")
-    if type(m_b) is not list:
-        raise TypeError("m_b must be a list")
-    for i in range(len(m_a)):
-        if type(m_a[i]) is not list:
-            raise TypeError("m_a must be a list of lists")
-    for i in range(len(m_b)):
-        if type(m_b[i]) is not list:
-            raise TypeError("m_b must be a list of lists")
-    if m_a == [] or m_a == [[]]:
-        raise ValueError("m_a can't be empty")
-    if m_b == [] or m_b == [[]]:
-        raise ValueError("m_b can't be empty")
-    size = len(m_a[0])
-    for i in range(len(m_a)):
-        if size != len(m_a[i]):
-            raise TypeError("each row of m_a must be of the same size")
-        for j in range(len(m_a[0])):
-            if type(m_a[i][j]) is not int and\
-               type(m_a[i][j]) is not float:
-                raise TypeError("m_a should contain only integers or floats")
-    size = len(m_b[0])
-    for i in range(len(m_b)):
-        if size != len(m_b[i]):
-            raise TypeError("each row of m_b must be of the same size")
-        for j in range(len(m_b[0])):
-            if type(m_b[i][j]) is not int and\
-               type(m_b[i][j]) is not float:
-                raise TypeError("m_b should contain only integers or floats")
-    if len(m_a[0]) != len(m_b):
-        raise ValueError("m_a and m_b can't be multiplied")
-    
-    return np.dot(m_a, m_b)
+    return np.matmul(m_a, m_b)
