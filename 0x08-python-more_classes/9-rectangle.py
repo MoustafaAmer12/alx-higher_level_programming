4ee2af42b6ec6c7d09cc6665292403ba2db6a554
MoustafaAmer12 <moustafaesam20113@gmail.com>
2024-04-23 05:09:29 +0200

    Task 9 edit

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -16,8 +16,8 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """Called upon creation of a new rectangle object
         """
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
