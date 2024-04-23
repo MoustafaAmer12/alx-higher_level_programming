0907b5212dbedc31b2afef40957db9337b287c8a
MoustafaAmer12 <moustafaesam20113@gmail.com>
2024-04-23 20:50:27 +0200

    Tasks 5 & 6

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A Module that declares a base geometry class
+"""
+
+
+class BaseGeometry():
+    """
+    A Class that calculates the area
+    """
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+A Module that declares a base geometry class
+"""
+
+
+class BaseGeometry:
+    """
+    A Class that calculates the area
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
