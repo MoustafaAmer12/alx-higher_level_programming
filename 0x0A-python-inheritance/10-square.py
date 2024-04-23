a149f652cb4e52fc77528083970c2fcdcb201516
MoustafaAmer12 <moustafaesam20113@gmail.com>
2024-04-23 22:42:14 +0200

    Task 10 edit

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -11,3 +11,7 @@ class Square(Rectangle):
     """
     def __init__(self, size):
         super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
