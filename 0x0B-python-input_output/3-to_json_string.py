e5402798cf11aadbbd56209b5b7729089935e95a
MoustafaAmer12 <moustafaesam20113@gmail.com>
2024-04-25 18:20:33 +0200

    Task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+A Module that creates a function to serialize 
+an object into a json string
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """A Function that serializes an object
+    into a JSON string
+    Returns:
+        JSON serialization of a string
+    """
+    return json.dumps(my_obj)
