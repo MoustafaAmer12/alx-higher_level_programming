2ffcdbb6aa2d3d562f9f1f1fd7f375c9e47c0468
MoustafaAmer12 <moustafaesam20113@gmail.com>
2024-04-25 17:28:09 +0200

    Task 0 doc edit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,9 @@ read a text file
 
 
 def read_file(filename=""):
+    """A function that reads a file using the utf-8
+    encoding and prints the data in the file.
+    """
     f_data = ""
     with open(filename, encoding="utf-8") as file:
         f_data = file.read()
