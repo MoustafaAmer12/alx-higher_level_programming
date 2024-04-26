0950c681fd384f9fa6a3540491b15f005a7ba101
MoustafaAmer12 <moustafaesam20113@gmail.com>
2024-04-26 04:48:57 +0300

    Task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+A Module that defines a student class
+with a student's name and age
+"""
+
+
+class Student:
+    """A Student class
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        list_str = True
+        if type(attrs) is not list:
+            list_str = False
+        else:
+            for i in attrs:
+                if type(i) is not str:
+                    list_str = False
+        if not list_str:
+            return self.__dict__
+        new_dict = dict()
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                new_dict[key] = value
+        return new_dict
+
+    def reload_from_json(self, json):
+        for key, val in json.items():
+            self.key = val
